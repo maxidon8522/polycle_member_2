9e530bd3822f7cad17ac629ebe58859734c889bb
maxidon8522 <maximurakami@icloud.com>
2025-11-01 14:59:16 +0800

    trigger build

--- web/src/types/next-auth.d.ts
+++ web/src/types/next-auth.d.ts
@@ -1,5 +1,7 @@
+import { DefaultSession } from "next-auth";
+
 declare module "next-auth" {
-  interface Session {
+  interface Session extends DefaultSession {
     slackUserId?: string | null;
     slackTeamId?: string | null;
     slackUserAccessToken?: string | null;
